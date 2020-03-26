e7979b236151562167af7483a6d0526aefe9a930
Navneet <kaur.navneet094@gmail.com>
2020-03-25 22:17:51 -0400

    test

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 <html>
 <head>
 	<title>Portfolio</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+	<div>
 	<h1>Navneet Kaur</h1>
+	</div>
 </body>
 </html>
