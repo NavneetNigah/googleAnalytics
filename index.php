ebe4e1d14102741a29748b36f2719eff35720fa9
Navneet <kaur.navneet094@gmail.com>
2020-03-25 23:52:10 -0400

    test 2

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div>
+	<div class="jumbotron">
 	<h1>Navneet Kaur</h1>
+	<i>This is going to be my portfolio</i>
 	</div>
 </body>
 </html>
